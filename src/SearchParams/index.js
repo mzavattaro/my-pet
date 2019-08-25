a9b288b4e40596341e15975cda2c42a831887dfe
Michael Zavattaro <mwzavattaro@gmail.com>
2019-08-25 20:57:34 +1000

    made dropdown a component and created custome hooks

--- src/SearchParams/index.js
+++ src/SearchParams/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from '../useDropdown/index';
 import print from '../utilities/print';
 
 const SearchParams = () => {
     const [location, setLocation] = useState('Seattle, WA');
-    const [animal, setAnimal] = useState('Dog');
+    const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
+    const [breeds, setBreeds] = useState([]);
+    const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
 
     print('state of location', location);
 
@@ -13,18 +16,15 @@ const SearchParams = () => {
             <h1>{location}</h1>
             <h1>{animal}</h1>
             <form>
+
                 <label htmlFor="location">
                     Location
                     <input id="location" value={location} placeholder="Location" onChange={event => setLocation(event.target.value)} />
                 </label>
-                <label htmlFor="animal">
-                    Animal
-                    <select id="animal" value={animal} onChange={event => setAnimal(event.target.value)} onBlur={event => setAnimal(event.target.value)}>
-                        <option>All</option>
-                        {ANIMALS.map(animal => (<option value={animal}>{animal}</option>))}
-                    </select>
 
-                </label>
+                <AnimalDropdown />
+                <BreedDropdown />
+
                 <button type="submit">Submit</button>
             </form>
         </div>
