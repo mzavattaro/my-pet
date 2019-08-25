f122594ed07753b32275ef1976825ef21fd351c2
Michael Zavattaro <mwzavattaro@gmail.com>
2019-08-25 17:56:40 +1000

    updated pet component to jsx

--- src/pet/index.js
+++ src/pet/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Pet = ({ name, animal, breed }) => React.createElement('div', {}, [
-    React.createElement('hi', {}, name),
-    React.createElement('hi', {}, animal),
-    React.createElement('hi', {}, breed),
-]);
+const Pet = ({ name, animal, breed }) => (
+    <div>
+        <h1>{name}</h1>
+        <h2>{animal}</h2>
+        <h2>{breed}</h2>
+    </div>
+);
 
 export default Pet;
 
