417fdcea36fef75fd10a5ab198590d34d5902a3f
Michael Zavattaro <mwzavattaro@gmail.com>
2019-08-25 17:33:02 +1000

    added proptypes to pet component

--- src/pet/index.js
+++ src/pet/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Pet = ({ name, animal, breed }) => {
-    <div>
-        Hello World
-    </div>;
-};
+const Pet = ({ name, animal, breed }) => React.createElement('div', {}, [
+    React.createElement('hi', {}, name),
+    React.createElement('hi', {}, animal),
+    React.createElement('hi', {}, breed),
+]);
 
 export default Pet;
+
+Pet.propTypes = {
+    name: PropTypes.string.isRequired,
+    animal: PropTypes.string.isRequired,
+    breed: PropTypes.string.isRequired,
+};
