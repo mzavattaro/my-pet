800afecb7b28b9c7b40d0477c95e71a6dc914950
Michael Zavattaro <mwzavattaro@gmail.com>
2019-08-25 17:25:50 +1000

    created new pet component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Pet from './pet';
 import 'normalize.css';
 import './App.css';
 
@@ -7,7 +8,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                Hello World
+                <h1>Adopt Me!</h1>
+                <Pet />
             </div>
         );
     }
---
+++ src/pet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Pet = ({ name, animal, breed }) => {
+    <div>
+        Hello World
+    </div>;
+};
+
+export default Pet;
